7f3109a7e2427b26c56b9f66914b7ab64e5e4e50
leanseefeld <leanseefeld@gmail.com>
2014-12-02 02:56:23 -0200

    [GUniver Manager] ProvasSynchronizer

---
+++ guniver-ui-admin/src/br/furb/guniver/sync/ProvasSynchronizer.java
@@ -0,0 +1,38 @@
+package br.furb.guniver.sync;
+
+import java.util.Collection;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import br.furb.guniver.modelo.Prova;
+
+public class ProvasSynchronizer extends EntitiesSynchronizer<Prova> {
+
+	public ProvasSynchronizer(String moduleUrl, ThreadPoolExecutor executor) {
+		super(moduleUrl, executor);
+	}
+
+	@Override
+	protected void doDownload(Prova entityAccessor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void doDownloadAll() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void doUpload(Prova entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void doUploadAll(Collection<Prova> entities) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
