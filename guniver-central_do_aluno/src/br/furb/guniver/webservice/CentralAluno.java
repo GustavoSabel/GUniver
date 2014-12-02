c818912a385291f4cda5a19fac77b73156ec1e5e
gsabel <gsabel@MC-3343.furb.br>
2014-12-02 21:35:10 -0200

    Adicionado mensagens de execução

--- guniver-central_do_aluno/src/br/furb/guniver/webservice/CentralAluno.java
+++ guniver-central_do_aluno/src/br/furb/guniver/webservice/CentralAluno.java
@@ -23,92 +23,119 @@ import br.furb.guniver.modulo.Financeiro;
 @WebService
 public class CentralAluno {
 
-    @WebMethod
-    public void removerCompromisso(long codigoCompromisso) {
-	BancoDados.getInstance().deleteCompromisso(codigoCompromisso);
-    }
-
-    @WebMethod
-    public Compromisso criarCompromisso(Compromisso compromisso) {
-	return BancoDados.getInstance().setCompromisso(compromisso);
-    }
-
-    @WebMethod
-    public Collection<Compromisso> getCompromissos() {
-	return BancoDados.getInstance().getCompromissos();
-    }
-
-    @WebMethod
-    public Collection<Compromisso> getCompromissosAluno(long codigoAluno) {
-	return BancoDados.getInstance().getCompromissos(codigoAluno);
-    }
-
-    @WebMethod
-    public String getMaterial(String idMaterial) {
-	return "http://ava.furb.br/ava/resources/tela_view.php?ds_diretorio=2666732&nm_arquivo=estados_UML.doc";
-    }
-
-    @WebMethod
-    public Collection<Prova> getProvas(int codigoAluno, int codigoTurma) throws Exception {
-	Academico academico = new Academico(Endereco.academico.getEndereco());
-	return ConversorAcademico.castProvas(academico.getProvas(codigoAluno, codigoTurma));
-    }
-
-    @WebMethod
-    public Collection<Matricula> getMatriculasAluno(int codigoAluno) throws Exception {
-	Academico academico = new Academico(Endereco.academico.getEndereco());
-	return ConversorAcademico.castMatriculas(academico.getMatriculasAluno(codigoAluno));
-    }
-
-    @WebMethod
-    public Collection<Matricula> getMatriculasTurma(int codigoTurma) throws Exception {
-	Academico academico = new Academico(Endereco.academico.getEndereco());
-	return ConversorAcademico.castMatriculas(academico.getMatriculasTurma(codigoTurma));
-    }
-
-    @WebMethod
-    public Collection<Horario> getHorarios(int codigoTurma) throws Exception {
-	Academico academico = new Academico(Endereco.academico.getEndereco());
-	return ConversorAcademico.castHorarios(academico.getHorarios(codigoTurma));
-    }
-
-    @WebMethod
-    public List<Aluno> getAlunos() throws RemoteException {
-	return Cadastro.getInstancia().getAlunos();
-    }
-
-    @WebMethod
-    public Curso getCurso(int codigoCurso) throws RemoteException {
-	return Cadastro.getInstancia().getCurso(codigoCurso);
-    }
-
-    @WebMethod
-    public List<Disciplina> getDisciplinas(int codigoCurso) throws RemoteException {
-	return Cadastro.getInstancia().getDisciplinas(codigoCurso);
-    }
-
-    @WebMethod
-    public List<Turma> getTurmas(int codigoAluno) throws RemoteException {
-	return Cadastro.getInstancia().getTurmas(codigoAluno);
-    }
-
-    @WebMethod
-    public List<Aluno> getAlunosTurma(int codigoTurma) throws RemoteException {
-	return Cadastro.getInstancia().getAlunosTurma(codigoTurma);
-    }
-
-    @WebMethod
-    public List<Mensalidade> getMensalidadesPendentes(int codigoAluno) throws RemoteException {
-	return Financeiro.getInstancia().getMensalidadesPendentes(codigoAluno);
-    }
-
-    @WebMethod
-    public Aluno autenticaUsuario(String nomeUsuario, String senha) throws RemoteException {
-	Aluno aluno = Cadastro.getInstancia().getAlunoPorNomeUsuario(nomeUsuario);
-	if (aluno != null && aluno.getSenha() == senha) {
-	    return aluno;
-	} else {
-	    return null;
+	@WebMethod
+	public void removerCompromisso(long codigoCompromisso) {
+		System.out.println("removerCompromisso");
+		BancoDados.getInstance().deleteCompromisso(codigoCompromisso);
 	}
-    }
+
+	@WebMethod
+	public Compromisso criarCompromisso(Compromisso compromisso) {
+		System.out.println("criarCompromisso");
+		return BancoDados.getInstance().setCompromisso(compromisso);
+	}
+
+	@WebMethod
+	public Collection<Compromisso> getCompromissos() {
+		System.out.println("getCompromissos");
+		return BancoDados.getInstance().getCompromissos();
+	}
+
+	@WebMethod
+	public Collection<Compromisso> getCompromissosAluno(long codigoAluno) {
+		System.out.println("getCompromissosAluno");
+		return BancoDados.getInstance().getCompromissos(codigoAluno);
+	}
+
+	@WebMethod
+	public String getMaterial(String idMaterial) {
+		System.out.println("getMaterial");
+		return "http://ava.furb.br/ava/resources/tela_view.php?ds_diretorio=2666732&nm_arquivo=estados_UML.doc";
+	}
+
+	@WebMethod
+	public Collection<Prova> getProvas(int codigoAluno, int codigoTurma)
+			throws Exception {
+		System.out.println("getProvas");
+		Academico academico = new Academico(Endereco.academico.getEndereco());
+		return ConversorAcademico.castProvas(academico.getProvas(codigoAluno,
+				codigoTurma));
+	}
+
+	@WebMethod
+	public Collection<Matricula> getMatriculasAluno(int codigoAluno)
+			throws Exception {
+		System.out.println("getMatriculasAluno");
+		Academico academico = new Academico(Endereco.academico.getEndereco());
+		return ConversorAcademico.castMatriculas(academico
+				.getMatriculasAluno(codigoAluno));
+	}
+
+	@WebMethod
+	public Collection<Matricula> getMatriculasTurma(int codigoTurma)
+			throws Exception {
+		System.out.println("getMatriculasTurma");
+		Academico academico = new Academico(Endereco.academico.getEndereco());
+		return ConversorAcademico.castMatriculas(academico
+				.getMatriculasTurma(codigoTurma));
+	}
+
+	@WebMethod
+	public Collection<Horario> getHorarios(int codigoTurma) throws Exception {
+		System.out.println("getHorarios");
+		Academico academico = new Academico(Endereco.academico.getEndereco());
+		return ConversorAcademico.castHorarios(academico
+				.getHorarios(codigoTurma));
+	}
+
+	@WebMethod
+	public List<Aluno> getAlunos() throws RemoteException {
+		System.out.println("getAlunos");
+		return Cadastro.getInstancia().getAlunos();
+	}
+
+	@WebMethod
+	public Curso getCurso(int codigoCurso) throws RemoteException {
+		System.out.println("getCurso");
+		return Cadastro.getInstancia().getCurso(codigoCurso);
+	}
+
+	@WebMethod
+	public List<Disciplina> getDisciplinas(int codigoCurso)
+			throws RemoteException {
+		System.out.println("getDisciplinas");
+		return Cadastro.getInstancia().getDisciplinas(codigoCurso);
+	}
+
+	@WebMethod
+	public List<Turma> getTurmas(int codigoAluno) throws RemoteException {
+		System.out.println("getTurmas");
+		return Cadastro.getInstancia().getTurmas(codigoAluno);
+	}
+
+	@WebMethod
+	public List<Aluno> getAlunosTurma(int codigoTurma) throws RemoteException {
+		System.out.println("getAlunosTurma");
+		return Cadastro.getInstancia().getAlunosTurma(codigoTurma);
+	}
+
+	@WebMethod
+	public List<Mensalidade> getMensalidadesPendentes(int codigoAluno)
+			throws RemoteException {
+		System.out.println("getMensalidadesPendentes");
+		return Financeiro.getInstancia().getMensalidadesPendentes(codigoAluno);
+	}
+
+	@WebMethod
+	public Aluno autenticaUsuario(String nomeUsuario, String senha)
+			throws RemoteException {
+		System.out.println("autenticaUsuario");
+		Aluno aluno = Cadastro.getInstancia().getAlunoPorNomeUsuario(
+				nomeUsuario);
+		if (aluno != null && aluno.getSenha() == senha) {
+			return aluno;
+		} else {
+			return null;
+		}
+	}
 }
