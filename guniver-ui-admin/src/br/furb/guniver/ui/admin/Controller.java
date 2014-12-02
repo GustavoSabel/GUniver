0aa2cf684880c1b7e66cac00f65d3a67d043ecc9
leanseefeld <leanseefeld@gmail.com>
2014-12-02 02:57:52 -0200

    [GUniver Manager] Config. dos SyncListeners e geração de seus stubs

--- guniver-ui-admin/src/br/furb/guniver/ui/admin/Controller.java
+++ guniver-ui-admin/src/br/furb/guniver/ui/admin/Controller.java
@@ -13,21 +13,24 @@ import javax.swing.JFrame;
 import br.furb.guniver.modelo.Aluno;
 import br.furb.guniver.modelo.Curso;
 import br.furb.guniver.modelo.Disciplina;
+import br.furb.guniver.modelo.Prova;
 import br.furb.guniver.modelo.Turma;
 import br.furb.guniver.sync.AlunosSynchronizer;
 import br.furb.guniver.sync.CursosSynchronizer;
 import br.furb.guniver.sync.DisciplinasSynchronizer;
 import br.furb.guniver.sync.EntitiesSynchronizer;
+import br.furb.guniver.sync.ProvasSynchronizer;
 import br.furb.guniver.sync.SyncListener;
 import br.furb.guniver.sync.TurmasSynchronizer;
 
 public class Controller {
 
-	private static Class<?>[] ENTITIES_CLASSES = { Aluno.class, Curso.class, Disciplina.class, Turma.class };
+	private static Class<?>[] ENTITIES_CLASSES = { Aluno.class, Curso.class, Disciplina.class, Prova.class, Turma.class };
 
-	public static int THREAD_POOL_MAX_SIZE = 4;
+	public static int THREAD_POOL_MAX_SIZE = 5;
 
-	private Map<Class<?>, EntitiesSynchronizer<?>> synchronizers = new HashMap<>(5);
+	private Map<Class<?>, EntitiesSynchronizer<?>> synchronizers;
+	private Map<Class<?>, SyncListener<?>> listeners;
 	private Collection<Aluno> alunos;
 	private Collection<Curso> cursos;
 	private Collection<Disciplina> disciplinas;
@@ -38,11 +41,17 @@ public class Controller {
 	private SyncListener<?> syncListener;
 
 	public Controller() {
-		synchronizers = new HashMap<Class<?>, EntitiesSynchronizer<?>>();
+		synchronizers = new HashMap<>();
+		listeners = new HashMap<>();
+
+		listeners.put(Aluno.class, new AlunoSyncListener());
+		listeners.put(Curso.class, new CursoSyncListener());
+		listeners.put(Disciplina.class, new DisciplinaSyncListener());
+		listeners.put(Prova.class, new ProvaSyncListener());
+		listeners.put(Turma.class, new TurmaSyncListener());
 	}
 
 	/**
-	 * 
 	 * @param synchronizers
 	 *            mapa com os sincronizadores de todos módulos do Sistema
 	 *            Distribuído. A chave é a classe da entidade e o valor é o
@@ -54,14 +63,21 @@ public class Controller {
 
 	public void setSynchronizers(Map<Class<?>, EntitiesSynchronizer<?>> synchronizers) {
 		synchronized (this.synchronizers) {
-			for (Class<?> entityClass : ENTITIES_CLASSES) {
-				EntitiesSynchronizer<?> synchronizer = synchronizers.get(entityClass);
-				if (synchronizer != null) {
-					// TODO: adicionar listener
-				}
-				this.synchronizers.put(entityClass, synchronizer);
-			}
+			registerSynchronizer(Aluno.class, synchronizers);
+			registerSynchronizer(Curso.class, synchronizers);
+			registerSynchronizer(Disciplina.class, synchronizers);
+			registerSynchronizer(Prova.class, synchronizers);
+			registerSynchronizer(Turma.class, synchronizers);
 		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T> void registerSynchronizer(Class<T> entityClass, Map<Class<?>, EntitiesSynchronizer<?>> synchronizers) {
+		EntitiesSynchronizer<T> synchronizer = (EntitiesSynchronizer<T>) synchronizers.get(entityClass);
+		if (synchronizer != null) {
+			synchronizer.addSyncListener((SyncListener<T>) listeners.get(entityClass));
+		}
+		this.synchronizers.put(entityClass, synchronizer);
 	}
 
 	/**
@@ -98,7 +114,7 @@ public class Controller {
 			synchronizer = synchronizers.get(entityClass);
 		}
 		if (synchronizer == null) {
-			throw new IllegalStateException(String.format("sincronizador da entidade %s não definido", entityClass.getName()));
+			throw new IllegalStateException(String.format("sincronizador da entidade %s não definido", entityClass.getSimpleName()));
 		}
 		return synchronizer;
 	}
@@ -135,18 +151,16 @@ public class Controller {
 		this.mainWindow = mainWindow;
 	}
 
-	public void changeUrls(String academicoUrl, String cadastrosUrl, String centralDoAlunoUrl, String financeiroUrl) {
+	public void changeUrls(String academicoUrl, String cadastrosUrl) {
 		stopSynchronizers();
 		synchronized (synchronizers) {
 			ThreadPoolExecutor executor = new ThreadPoolExecutor(THREAD_POOL_MAX_SIZE, THREAD_POOL_MAX_SIZE, 1, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>());
-			// TODO: rever URLs
 			synchronizers.put(Aluno.class, new AlunosSynchronizer(cadastrosUrl, executor));
 			synchronizers.put(Curso.class, new CursosSynchronizer(cadastrosUrl, executor));
 			synchronizers.put(Disciplina.class, new DisciplinasSynchronizer(cadastrosUrl, executor));
 			synchronizers.put(Turma.class, new TurmasSynchronizer(cadastrosUrl, executor));
+			synchronizers.put(Prova.class, new ProvasSynchronizer(academicoUrl, executor));
 		}
-		// TODO Auto-generated method stub
-
 	}
 
 	private void stopSynchronizers() {
@@ -155,6 +169,141 @@ public class Controller {
 				synchronizer.removeSyncListener(syncListener);
 				synchronizer.stop();
 			}
+		}
+	}
+
+	private class AlunoSyncListener implements SyncListener<Aluno> {
+
+		@Override
+		public void downloadAllComplete(Collection<Aluno> entities) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void downloadComplete(Aluno downloadedEntity) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void uploadComplete(Aluno uploadedEntity) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void syncFailed(Throwable reason) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+
+	private class CursoSyncListener implements SyncListener<Curso> {
+
+		@Override
+		public void downloadAllComplete(Collection<Curso> entities) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void downloadComplete(Curso downloadedEntity) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void uploadComplete(Curso uploadedEntity) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void syncFailed(Throwable reason) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+
+	private class DisciplinaSyncListener implements SyncListener<Disciplina> {
+
+		@Override
+		public void downloadAllComplete(Collection<Disciplina> entities) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void downloadComplete(Disciplina downloadedEntity) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void uploadComplete(Disciplina uploadedEntity) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void syncFailed(Throwable reason) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+
+	private class ProvaSyncListener implements SyncListener<Prova> {
+
+		@Override
+		public void downloadAllComplete(Collection<Prova> entities) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void downloadComplete(Prova downloadedEntity) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void uploadComplete(Prova uploadedEntity) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void syncFailed(Throwable reason) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+
+	private class TurmaSyncListener implements SyncListener<Turma> {
+
+		@Override
+		public void downloadAllComplete(Collection<Turma> entities) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void downloadComplete(Turma downloadedEntity) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void uploadComplete(Turma uploadedEntity) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void syncFailed(Throwable reason) {
+			// TODO Auto-generated method stub
+
 		}
 	}
 
