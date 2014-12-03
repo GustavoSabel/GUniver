67b935df3dd3c94e3e2a6c206e966ed1ac6234f4
leanseefeld <leanseefeld@gmail.com>
2014-12-03 10:08:27 -0200

    TurmasSynchronizer: downloadAllByAluno

--- guniver-ui-sync/src/br/furb/guniver/sync/TurmasSynchronizer.java
+++ guniver-ui-sync/src/br/furb/guniver/sync/TurmasSynchronizer.java
@@ -6,62 +6,80 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
+
 import br.furb.guniver.Conversor;
+import br.furb.guniver.central_do_aluno.stubs.Aluno;
 import br.furb.guniver.central_do_aluno.stubs.CentralAluno;
 import br.furb.guniver.central_do_aluno.stubs.CentralAlunoService;
 import br.furb.guniver.central_do_aluno.stubs.Turma;
-import br.furb.guniver.modelo.Disciplina;
-import br.furb.guniver.rmi.AlunoRemote;
 import br.furb.guniver.rmi.TurmaRemote;
 
 public class TurmasSynchronizer extends EntitiesSynchronizer<Turma> {
 
-    private TurmaRemote turmaRemote;
-    private CentralAluno centralAluno;
+	private TurmaRemote turmaRemote;
+	private CentralAluno centralAluno;
 
-    public TurmasSynchronizer(String moduleUrl, ThreadPoolExecutor executor) {
-	super(moduleUrl, executor);
-	try {
-	    URL url = new URL("http://" + moduleUrl + ":8080/centralAluno");
-	    turmaRemote = (TurmaRemote) Naming.lookup("//" + moduleUrl + "/TurmaRemote");
+	public TurmasSynchronizer(String moduleUrl, ThreadPoolExecutor executor) {
+		super(moduleUrl, executor);
+		try {
+			URL url = new URL("http://" + moduleUrl + ":8080/centralAluno");
+			turmaRemote = (TurmaRemote) Naming.lookup("//" + moduleUrl + "/TurmaRemote");
 
-	    centralAluno = new CentralAlunoService(url).getCentralAlunoPort();
-	} catch (Exception ex) {
-	    throw new RuntimeException("Erro ao conectar com " + moduleUrl, ex);
+			centralAluno = new CentralAlunoService(url).getCentralAlunoPort();
+		} catch (Exception ex) {
+			throw new RuntimeException("Erro ao conectar com " + moduleUrl, ex);
+		}
 	}
-    }
 
-    @Override
-    protected void doDownload(Turma entityAccessor) {
-	entityAccessor.setAno(2012);
-	entityAccessor.setSemestre(1);
-    }
-
-    @Override
-    protected Collection<Turma> doDownloadAll() {
-	try {
-	    List<Turma> turmas = new ArrayList<Turma>();
-	    for (br.furb.guniver.modelo.Turma turmaWS : turmaRemote.getTurmas()) {
-		turmas.add(Conversor.cast(turmaWS));
-	    }
-
-	    return turmas;
-	} catch (Exception ex) {
-	    throw new RuntimeException(ex);
+	@Override
+	protected void doDownload(Turma entityAccessor) {
+		entityAccessor.setAno(2012);
+		entityAccessor.setSemestre(1);
 	}
-    }
 
-    @Override
-    protected void doUpload(Turma entity) {
-	// TODO Auto-generated method stub
+	@Override
+	protected Collection<Turma> doDownloadAll() {
+		try {
+			List<Turma> turmas = new ArrayList<Turma>();
+			for (br.furb.guniver.modelo.Turma turmaWS : turmaRemote.getTurmas()) {
+				turmas.add(Conversor.cast(turmaWS));
+			}
 
-    }
+			return turmas;
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 
-    @Override
-    protected void doUploadAll(Collection<Turma> entities) {
-	// TODO Auto-generated method stub
+	@Override
+	protected void doUpload(Turma entity) {
+		// TODO Auto-generated method stub
 
-    }
+	}
+
+	@Override
+	protected void doUploadAll(Collection<Turma> entities) {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected Collection<Turma> doDownloadAllByAluno(Aluno aluno) {
+		// TODO Auto-generated method stub
+
+		return Collections.emptyList();
+	}
+
+	public Future<?> downloadAllByAluno(Aluno aluno) {
+		SynchronizerTask<Aluno> downloadAllTask = new SynchronizerTask<Aluno>(aluno) {
+			@Override
+			void doTask(Aluno parameter) {
+				fireDownloadAllComplete(doDownloadAllByAluno(parameter));
+			}
+		};
+
+		return submit(downloadAllTask);
+	}
 
 }
