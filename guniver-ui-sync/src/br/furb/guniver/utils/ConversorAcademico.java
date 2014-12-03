5d309ead021b48e0656506d5aea347e1ea377dff
leanseefeld <leanseefeld@gmail.com>
2014-12-03 08:28:03 -0200

    [FIX] NullPointer ao converter prova recem criada

--- guniver-ui-sync/src/br/furb/guniver/utils/ConversorAcademico.java
+++ guniver-ui-sync/src/br/furb/guniver/utils/ConversorAcademico.java
@@ -4,9 +4,10 @@ import br.furb.guniver.modelo.academico.Prova;
 
 public class ConversorAcademico {
 
-    public static br.furb.guniver.modelo.academico.Prova cast(br.furb.guniver.central_do_aluno.stubs.Prova entity) {
-	return new Prova(0, entity.getDescricao(), entity.getAluno().getCodigo(), entity.getTurma().getCodigo(),
-		entity.getNota());
-    }
+	public static br.furb.guniver.modelo.academico.Prova cast(br.furb.guniver.central_do_aluno.stubs.Prova entity) {
+		int codAluno = entity.getAluno() == null ? 0 : entity.getAluno().getCodigo();
+		int codTurma = entity.getTurma() == null ? 0 : entity.getTurma().getCodigo();
+		return new Prova(0, entity.getDescricao(), codAluno, codTurma, entity.getNota());
+	}
 
 }
