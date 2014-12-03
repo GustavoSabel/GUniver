f4e7250d9d01524c088cc47d5e494a5927bb1141
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2014-12-03 14:57:44 -0200

    [ui-admini] alterado a ordem das colunas das provas

--- guniver-ui-admin/src/br/furb/guniver/ui/admin/ProvaFragment.java
+++ guniver-ui-admin/src/br/furb/guniver/ui/admin/ProvaFragment.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Vector;
-
 import javax.swing.DefaultListSelectionModel;
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -23,7 +22,6 @@ import javax.swing.event.ListSelectionListener;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
-
 import br.furb.guniver.central_do_aluno.stubs.Aluno;
 import br.furb.guniver.central_do_aluno.stubs.Prova;
 import br.furb.guniver.central_do_aluno.stubs.Turma;
@@ -32,202 +30,202 @@ import br.furb.guniver.ui.Fragment;
 @SuppressWarnings("serial")
 public class ProvaFragment extends Fragment {
 
-	private JTable tableProvas;
-	private Controller controller;
-	private DefaultTableModel dataModel;
-	private List<Prova> provas = new ArrayList<>();
-	private boolean fChangingData;
-	private Prova selectedProva;
+    private JTable tableProvas;
+    private Controller controller;
+    private DefaultTableModel dataModel;
+    private List<Prova> provas = new ArrayList<>();
+    private boolean fChangingData;
+    private Prova selectedProva;
 
-	public ProvaFragment(Controller controller) {
-		this.controller = controller;
-		controller.setProvaFragment(this);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 0, 0 };
-		gridBagLayout.rowHeights = new int[] { 0, 0, 0 };
-		gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-		gridBagLayout.rowWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
-		setLayout(gridBagLayout);
+    public ProvaFragment(Controller controller) {
+	this.controller = controller;
+	controller.setProvaFragment(this);
+	GridBagLayout gridBagLayout = new GridBagLayout();
+	gridBagLayout.columnWidths = new int[] { 0, 0 };
+	gridBagLayout.rowHeights = new int[] { 0, 0, 0 };
+	gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+	gridBagLayout.rowWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
+	setLayout(gridBagLayout);
 
-		JScrollPane scrollPaneProvas = new JScrollPane();
-		GridBagConstraints gbc_scrollPaneProvas = new GridBagConstraints();
-		gbc_scrollPaneProvas.insets = new Insets(5, 5, 5, 5);
-		gbc_scrollPaneProvas.fill = GridBagConstraints.BOTH;
-		gbc_scrollPaneProvas.gridx = 0;
-		gbc_scrollPaneProvas.gridy = 0;
-		add(scrollPaneProvas, gbc_scrollPaneProvas);
+	JScrollPane scrollPaneProvas = new JScrollPane();
+	GridBagConstraints gbc_scrollPaneProvas = new GridBagConstraints();
+	gbc_scrollPaneProvas.insets = new Insets(5, 5, 5, 5);
+	gbc_scrollPaneProvas.fill = GridBagConstraints.BOTH;
+	gbc_scrollPaneProvas.gridx = 0;
+	gbc_scrollPaneProvas.gridy = 0;
+	add(scrollPaneProvas, gbc_scrollPaneProvas);
 
-		tableProvas = new JTable();
-		dataModel = new DefaultTableModel(new Object[][] { { null, null, null,
-				null }, }, new String[] { "Aluno", "Turma", "Nota",
-				"Descri\u00E7\u00E3o" }) {
-			Class[] columnTypes = new Class[] { String.class, Long.class,
-					Float.class, String.class };
-			boolean[] columnEditable = { false, false, true, true };
+	tableProvas = new JTable();
+	dataModel = new DefaultTableModel(new Object[][] { { null, null, null, null }, }, new String[] {
+		"Descri\u00E7\u00E3o", "Aluno", "Turma", "Nota" }) {
 
-			@Override
-			public boolean isCellEditable(int row, int column) {
-				return columnEditable[column];
+	    Class[] columnTypes = new Class[] { String.class, Long.class, Long	.class, Float.class };
+	    boolean[] columnEditable = { true, false, false, true };
+
+	    @Override
+	    public boolean isCellEditable(int row, int column) {
+		return columnEditable[column];
+	    }
+
+	    public Class getColumnClass(int columnIndex) {
+		return columnTypes[columnIndex];
+	    }
+	};
+
+	dataModel.addTableModelListener(new TableModelListener() {
+
+	    @Override
+	    public void tableChanged(TableModelEvent e) {
+		if (!fChangingData && e.getType() == TableModelEvent.UPDATE) {
+		    int row = e.getFirstRow();
+		    int col = e.getColumn();
+		    Prova prova;
+		    boolean newProva;
+		    if (row == provas.size()) {
+			prova = new Prova();
+			newProva = true;
+		    } else {
+			prova = provas.get(row);
+			newProva = false;
+		    }
+		    Object newValue = dataModel.getValueAt(row, col);
+		    Object oldValue;
+		    switch (col) {
+			case 0:
+			    oldValue = prova.getDescricao();
+			    prova.setDescricao((String) newValue);
+			    break;
+			case 3:
+			    oldValue = prova.getNota();
+			    prova.setNota((Float) newValue);
+			    break;
+			default:
+			    oldValue = null; // always different
+			    break;
+		    }
+		    if (!newValue.equals(oldValue)) {
+			if (newProva) {
+			    provas.add(prova);
+			    reloadTable();
 			}
-
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
-			}
-		};
-
-		dataModel.addTableModelListener(new TableModelListener() {
-
-			@Override
-			public void tableChanged(TableModelEvent e) {
-				if (!fChangingData && e.getType() == TableModelEvent.UPDATE) {
-					int row = e.getFirstRow();
-					int col = e.getColumn();
-					Prova prova;
-					boolean newProva;
-					if (row == provas.size()) {
-						prova = new Prova();
-						newProva = true;
-					} else {
-						prova = provas.get(row);
-						newProva = false;
-					}
-					Object newValue = dataModel.getValueAt(row, col);
-					Object oldValue;
-					switch (col) {
-					case 2:
-						oldValue = prova.getNota();
-						prova.setNota((Float) newValue);
-						break;
-					case 3:
-						oldValue = prova.getDescricao();
-						prova.setDescricao((String) newValue);
-						break;
-					default:
-						oldValue = null; // always different
-						break;
-					}
-					if (!newValue.equals(oldValue)) {
-						if (newProva) {
-							provas.add(prova);
-							reloadTable();
-						}
-						ProvaFragment.this.controller.uploadProva(prova);
-					}
-				}
-			}
-		});
-
-		DefaultListSelectionModel selModel = new DefaultListSelectionModel();
-		selModel.addListSelectionListener(new ListSelectionListener() {
-			@Override
-			public void valueChanged(ListSelectionEvent e) {
-				int index = e.getFirstIndex();
-				if (index < 0 || index >= provas.size()) {
-					selectedProva = null;
-				} else {
-					selectedProva = provas.get(index);
-				}
-			}
-		});
-		tableProvas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		tableProvas.setSelectionModel(selModel);
-
-		tableProvas.setModel(dataModel);
-		scrollPaneProvas.setViewportView(tableProvas);
-
-		JPanel panelManageProva = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) panelManageProva.getLayout();
-		flowLayout.setAlignment(FlowLayout.RIGHT);
-		flowLayout.setVgap(0);
-		GridBagConstraints gbc_panelManageProva = new GridBagConstraints();
-		gbc_panelManageProva.insets = new Insets(0, 5, 5, 0);
-		gbc_panelManageProva.fill = GridBagConstraints.BOTH;
-		gbc_panelManageProva.gridx = 0;
-		gbc_panelManageProva.gridy = 1;
-		add(panelManageProva, gbc_panelManageProva);
-
-		JButton btnRemover = new JButton("Remover");
-		btnRemover.setVisible(false);
-		btnRemover.setEnabled(false);
-		panelManageProva.add(btnRemover);
-
-		JButton btnAlterarAluno = new JButton("Alterar Aluno");
-		btnAlterarAluno.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (selectedProva != null) {
-					changeAluno(selectedProva);
-				}
-			}
-		});
-		panelManageProva.add(btnAlterarAluno);
-
-		JButton btnAlterarTurma = new JButton("Alterar Turma");
-		btnAlterarTurma.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (selectedProva != null) {
-					changeTurma(selectedProva);
-				}
-			}
-		});
-		panelManageProva.add(btnAlterarTurma);
-	}
-
-	@SuppressWarnings("unchecked")
-	public void setProvas(Collection<Prova> provas) {
-		fChangingData = true;
-		this.provas.clear();
-		this.provas.addAll(provas);
-		Vector<Vector<?>> dataVector = dataModel.getDataVector();
-		dataVector.clear();
-
-		for (Prova p : provas) {
-			Vector<Object> row = new Vector<>();
-			row.add(p.getAluno() == null ? "--" : p.getAluno().getNome());
-			row.add(p.getTurma() == null ? "--" : p.getTurma().getCodigo());
-			row.add(p.getNota());
-			row.add(p.getDescricao());
-			dataVector.add(row);
-		}
-		// empty row
-		dataVector.add(new Vector<>(Arrays.asList(null, null, null, null)));
-
-		dataModel.fireTableDataChanged();
-		fChangingData = false;
-	}
-
-	public void reloadTable() {
-		setProvas(new ArrayList<>(provas));
-	}
-
-	public void updateProva(Prova uploadedEntity) {
-		reloadTable();
-	}
-
-	private void changeAluno(Prova prova) {
-		Aluno pickedAluno = controller.pickAluno();
-		if (pickedAluno != null) {
-			prova.setAluno(pickedAluno);
-			updateProva(prova);
 			ProvaFragment.this.controller.uploadProva(prova);
+		    }
 		}
-	}
+	    }
+	});
 
-	private void changeTurma(Prova prova) {
-		Turma turma = controller.pickTurma();
-		if (turma != null) {
-			prova.setTurma(turma);
-			updateProva(prova);
-			ProvaFragment.this.controller.uploadProva(prova);
+	DefaultListSelectionModel selModel = new DefaultListSelectionModel();
+	selModel.addListSelectionListener(new ListSelectionListener() {
+
+	    @Override
+	    public void valueChanged(ListSelectionEvent e) {
+		int index = e.getFirstIndex();
+		if (index < 0 || index >= provas.size()) {
+		    selectedProva = null;
+		} else {
+		    selectedProva = provas.get(index);
 		}
-	}
+	    }
+	});
+	tableProvas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+	tableProvas.setSelectionModel(selModel);
 
-	@Override
-	public String getTitle() {
-		return "Provas";
+	tableProvas.setModel(dataModel);
+	scrollPaneProvas.setViewportView(tableProvas);
+
+	JPanel panelManageProva = new JPanel();
+	FlowLayout flowLayout = (FlowLayout) panelManageProva.getLayout();
+	flowLayout.setAlignment(FlowLayout.RIGHT);
+	flowLayout.setVgap(0);
+	GridBagConstraints gbc_panelManageProva = new GridBagConstraints();
+	gbc_panelManageProva.insets = new Insets(0, 5, 5, 0);
+	gbc_panelManageProva.fill = GridBagConstraints.BOTH;
+	gbc_panelManageProva.gridx = 0;
+	gbc_panelManageProva.gridy = 1;
+	add(panelManageProva, gbc_panelManageProva);
+
+	JButton btnRemover = new JButton("Remover");
+	btnRemover.setVisible(false);
+	btnRemover.setEnabled(false);
+	panelManageProva.add(btnRemover);
+
+	JButton btnAlterarAluno = new JButton("Alterar Aluno");
+	btnAlterarAluno.addActionListener(new ActionListener() {
+
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		if (selectedProva != null) {
+		    changeAluno(selectedProva);
+		}
+	    }
+	});
+	panelManageProva.add(btnAlterarAluno);
+
+	JButton btnAlterarTurma = new JButton("Alterar Turma");
+	btnAlterarTurma.addActionListener(new ActionListener() {
+
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		if (selectedProva != null) {
+		    changeTurma(selectedProva);
+		}
+	    }
+	});
+	panelManageProva.add(btnAlterarTurma);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void setProvas(Collection<Prova> provas) {
+	fChangingData = true;
+	this.provas.clear();
+	this.provas.addAll(provas);
+	Vector<Vector<?>> dataVector = dataModel.getDataVector();
+	dataVector.clear();
+
+	for (Prova p : provas) {
+	    Vector<Object> row = new Vector<>();
+	    row.add(p.getDescricao());
+	    row.add(p.getAluno() == null ? "--" : p.getAluno().getNome());
+	    row.add(p.getTurma() == null ? "--" : p.getTurma().getCodigo());
+	    row.add(p.getNota());
+	    dataVector.add(row);
 	}
+	// empty row
+	dataVector.add(new Vector<>(Arrays.asList(null, null, null, null)));
+
+	dataModel.fireTableDataChanged();
+	fChangingData = false;
+    }
+
+    public void reloadTable() {
+	setProvas(new ArrayList<>(provas));
+    }
+
+    public void updateProva(Prova uploadedEntity) {
+	reloadTable();
+    }
+
+    private void changeAluno(Prova prova) {
+	Aluno pickedAluno = controller.pickAluno();
+	if (pickedAluno != null) {
+	    prova.setAluno(pickedAluno);
+	    updateProva(prova);
+	    ProvaFragment.this.controller.uploadProva(prova);
+	}
+    }
+
+    private void changeTurma(Prova prova) {
+	Turma turma = controller.pickTurma();
+	if (turma != null) {
+	    prova.setTurma(turma);
+	    updateProva(prova);
+	    ProvaFragment.this.controller.uploadProva(prova);
+	}
+    }
+
+    @Override
+    public String getTitle() {
+	return "Provas";
+    }
 
 }
