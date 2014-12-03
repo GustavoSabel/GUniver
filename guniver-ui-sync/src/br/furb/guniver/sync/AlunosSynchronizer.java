cb95737ba360d3d3fcd47359e5e37eb824a959be
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2014-12-02 23:40:11 -0200

    [sync] Ajustado para o web service receber o IP informado pelo usuário

--- guniver-ui-sync/src/br/furb/guniver/sync/AlunosSynchronizer.java
+++ guniver-ui-sync/src/br/furb/guniver/sync/AlunosSynchronizer.java
@@ -1,46 +1,45 @@
 package br.furb.guniver.sync;
 
+import java.net.URL;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ThreadPoolExecutor;
-
 import br.furb.guniver.central_do_aluno.stubs.Aluno;
 import br.furb.guniver.central_do_aluno.stubs.CentralAluno;
 import br.furb.guniver.central_do_aluno.stubs.CentralAlunoService;
 
 public class AlunosSynchronizer extends EntitiesSynchronizer<Aluno> {
 
-	CentralAluno port;
-	
-	public AlunosSynchronizer(String moduleUrl, ThreadPoolExecutor executor) {
-		super(moduleUrl, executor);
-		port = new CentralAlunoService().getCentralAlunoPort();
-		
-	}
+    CentralAluno centralAluno;
 
-	@Override
-	protected void doDownload(Aluno entityAccessor) {
-		entityAccessor.setNome("Nome do aluno " + entityAccessor.getCodigo());
+    public AlunosSynchronizer(String moduleUrl, ThreadPoolExecutor executor) {
+	super(moduleUrl, executor);
+	try {
+	    URL url = new URL("http://" + moduleUrl + ":8080/centralAluno");
+	    centralAluno = new CentralAlunoService(url).getCentralAlunoPort();
+	} catch (Exception ex) {
+	    System.out.println("Erro ao conectar com " + moduleUrl);
+	    System.out.println(ex.getMessage());
 	}
+    }
 
-	@Override
-	protected Collection<Aluno> doDownloadAll() {
-		
-		List<Aluno> alunos = port.getAlunos();
-		
-		return alunos;
-		
-		/*return Arrays.asList(new Aluno(1, "joao123", "123456", "João"), // 
-				new Aluno(5, "pedroso", "qsenha", "Pedro Silva"), // 
-				new Aluno(234, "ramonin", "hoho2512", "Ramon Noel"));*/
-	}
+    @Override
+    protected void doDownload(Aluno entityAccessor) {
+	entityAccessor.setNome("Nome do aluno " + entityAccessor.getCodigo());
+    }
 
-	@Override
-	protected void doUpload(Aluno entity) {
-	}
+    @Override
+    protected Collection<Aluno> doDownloadAll() {
+	List<Aluno> alunos = centralAluno.getAlunos();
+	return alunos;
+    }
 
-	@Override
-	protected void doUploadAll(Collection<Aluno> entities) {
-	}
+    @Override
+    protected void doUpload(Aluno entity) {
+    }
+
+    @Override
+    protected void doUploadAll(Collection<Aluno> entities) {
+    }
 
 }
