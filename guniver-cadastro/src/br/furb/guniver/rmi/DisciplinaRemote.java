b5c4726a4ae491c58da1b6b8bda6e31a5d7f86b8
vivianpanzenhagen <vivian.panzenhagen@gmail.com>
2014-12-03 14:08:57 -0200

    Confirmando criação dos métodos que serão utilizados no WebService

--- guniver-cadastro/src/br/furb/guniver/rmi/DisciplinaRemote.java
+++ guniver-cadastro/src/br/furb/guniver/rmi/DisciplinaRemote.java
@@ -8,13 +8,10 @@ import br.furb.guniver.modelo.Disciplina;
 public interface DisciplinaRemote extends Remote {
 
     public List<Disciplina> getDisciplinas() throws RemoteException;
-
     public List<Disciplina> getDisciplinasCurso(int codCurso) throws RemoteException;
-
     public void cadastrarDisciplina(Disciplina disciplina) throws RemoteException;
-
+   //os métodos abaixo não aparecem no syncDisciplina
     public Disciplina getDisciplina(int codDisciplina) throws RemoteException;
-
     public List<Disciplina> getDisciplinasAll() throws RemoteException;
     //teste
 
